eebc921548841ba8070c899820d6452cdcbf79b6
Edio Ilha <edioilha@gmail.com>
2018-10-09 17:56:34 -0300

    Validação de formulário

--- app/Http/Requests/ContatoRequest.php
+++ app/Http/Requests/ContatoRequest.php
@@ -13,7 +13,7 @@ class ContatoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,42 @@ class ContatoRequest extends FormRequest
      */
     public function rules()
     {
+        switch ($this->method()){
+            case "POST": // Criação de um novo registro
+                return[
+                    'saudacao' => 'required|max:5',
+                    'nome' => 'required|max:100',
+                    'telefone' => 'required|max:25',
+                    'email' => 'email|max:200|unique:contatos',
+                    'data_nascimento' => 'data_format:"d/m/Y"',
+                    'saudacao' => 'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
+                ];
+            break;
+            case "PUT": // atulização
+                return[
+                    'saudacao' => 'required|max:5',
+                    'nome' => 'required|max:100',
+                    'telefone' => 'required|max:25',
+                    'email' => 'email|max:200|unique:contatos,email'.$this->id,
+                    'data_nascimento' => 'data_format:"d/m/Y"',
+                    'saudacao' => 'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
+                ];
+            break;
+
+            default:break;
+        }
         return [
             //
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'saudacao.required' => 'O campo saudação é obrgatório',
+            'nome.required'     => 'O campo nome é obrigatório',
+            'email.email'       => 'Informe um e-mai válido',
+            'data_nascimento.date_format' => 'O campo data deve ser no formato DD/MM/AAAA'
         ];
     }
 }
